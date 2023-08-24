29c0d6c68c29a37a7922871aff6ac2e6c824c9b3
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-08-25 01:42:22 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
             dependencies: [
                 .product(name: "BigInt", package: "BigInt"),
                 .product(name: "CryptoSwift", package: "CryptoSwift"),
-                .product(name: "secp256k1", package: "secp256k1"),
+                "secp256k1",
             ],
             path: "Sources",
             sources: ["Core", "FoundationHTTP"]),
